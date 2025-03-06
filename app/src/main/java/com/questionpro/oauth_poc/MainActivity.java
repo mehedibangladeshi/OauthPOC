13ac983c486f40a37186030e121fe41f026729ae
Mehedi Hasan <mehedi.hasan@questionpro.com>
2025-03-06 13:55:16 +0600

    Implemented Auth0

--- app/src/main/java/com/questionpro/oauth_poc/MainActivity.java
+++ app/src/main/java/com/questionpro/oauth_poc/MainActivity.java
@@ -1,81 +1,128 @@
 package com.questionpro.oauth_poc;
-import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 100;
-    private GoogleSignInClient mGoogleSignInClient;
-    private SignInButton btnGoogleSignIn;
+
+    private TextView resultTextView;
+    private static final String TAG = "MainActivity";
+    private static final String AUTH0_DOMAIN = "dev-udw0cpbqzi4xqeq2.us.auth0.com";
+    private static final String AUTH0_CLIENT_ID = "IzH7kdGPAQmfgQ8ynA1QlswU6zfml307";
+    private static final String AUTH0_CLIENT_SECRET = "uX_eJPfxdbdfh7B55zVgiMPZ-NRK_8ltZ3O6o8wMu3BMLg5Z3FnNMXfUQ9dSWJOs";
+    private static final String AUTH0_TOKEN_URL = "https://" + AUTH0_DOMAIN + "/oauth/token";
+    private static final String AUTH0_AUDIENCE = "https://dev-udw0cpbqzi4xqeq2.us.auth0.com/api/v2/";
+    private static final String AUTH0_API_URL = "https://dev-udw0cpbqzi4xqeq2.us.auth0.com/api/v2/users";
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    resultTextView = findViewById(R.id.tvResult);
+    Button authorizeButton = findViewById(R.id.btnAuthorize);
+
+
+    authorizeButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            new GetAccessTokenTask().execute();
+        }
+    });
+}
+
+private class GetAccessTokenTask extends AsyncTask<Void, Void, String> {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        EdgeToEdge.enable(this);
-        btnGoogleSignIn = findViewById(R.id.btnGoogleSignIn);
+    protected String doInBackground(Void... voids) {
+        OkHttpClient client = new OkHttpClient();
 
-        // 1. Configure sign-in to request the user’s ID, email, and basic profile
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .requestIdToken("391922013901-sribddnvg85cgl8mdkge4s9bg6vcapoe.apps.googleusercontent.com")
-                // If you want an ID token, add .requestIdToken("YOUR_WEB_CLIENT_ID")
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&client_id=" + AUTH0_CLIENT_ID
+                + "&client_secret=" + AUTH0_CLIENT_SECRET
+                + "&audience=" + AUTH0_AUDIENCE);
+        Request request = new Request.Builder()
+                .url(AUTH0_TOKEN_URL)
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
                 .build();
 
-        // 2. Build a GoogleSignInClient with the options
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                JSONObject jsonResponse = new JSONObject(responseBody);
+                String accessToken = jsonResponse.getString("access_token");
+                return accessToken;
 
-        // 3. Set onClick listener
-        btnGoogleSignIn.setOnClickListener(view -> signIn());
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            } else {
+                Log.e(TAG, "Error getting access token: " + response.code() + " " + response.body().string());
+                return null;
+            }
+        } catch (IOException | JSONException e) {
+            Log.e(TAG, "Error: " + e.getMessage());
+            return null;
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-        Log.d("sign", "Inside try");
-            if (account != null) {
-                String displayName = account.getDisplayName();
-                String email = account.getEmail();
-                Uri profilePic = account.getPhotoUrl();
-
-                Intent intent = new Intent(this, ProfileActivity.class);
-                intent.putExtra("name", displayName);
-                intent.putExtra("email", email);
-                if (profilePic != null) {
-                    intent.putExtra("photoUrl", profilePic.toString());
-                }
-                startActivity(intent);
-                finish();
-            }
-        } catch (ApiException e) {
-            // Handle failure
-            Log.w("SignIn", "signInResult:failed code=" + e.getStatusCode());
+    @Override
+    protected void onPostExecute(String accessToken) {
+        if (accessToken != null) {
+            resultTextView.setText("Access Token: " + accessToken);
+            new CallApiTask(accessToken).execute();
+        } else {
+            resultTextView.setText("Failed to get access token.");
         }
     }
 }
+
+private class CallApiTask extends AsyncTask<Void, Void, String> {
+
+    private final String accessToken;
+
+    CallApiTask(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(AUTH0_API_URL)
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            } else {
+                Log.e(TAG, "Error calling API: " + response.code() + " " + response.body().string());
+                return "API call failed: " + response.code();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "API call error: " + e.getMessage());
+            return "API call error: " + e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        resultTextView.setText("API Response: " + result);
+    }
+    }
+}
+
+
 
